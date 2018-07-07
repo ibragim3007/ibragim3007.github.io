a63de9f3e7d737a7b2e427914eb8a9f172772d09
ibragim3007 <40404277+ibragim3007@users.noreply.github.com>
2018-07-07 09:35:52 +0300

    asd

---
+++ images/script.js
@@ -0,0 +1,12 @@
+
+
+var modal = document.getElementById('mymodal');
+var btn = document.getElementById('open');
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function(){
+	modal.style.display = 'block'
+}
+span.onclick = function(){
+	modal.style.display = "none";
+}
